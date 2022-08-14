f11681f531657b1feb72b4da6f8aee67676e905a
Kirill <pnchkirill@gmail.com>
2022-08-14 13:57:10 +0300

    Third task completed

--- third_task/main.py
+++ third_task/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from common.utils import get_input_data
+from common.utils import get_input_data, save_data
 from third_task.questions_tree import get_question_tree
 
 
@@ -8,7 +8,8 @@ def main():
     """Запускает выполнение программы."""
     current_dir = Path(__file__).resolve().parent
     input_data = get_input_data(current_dir / 'questions.json')
-    questions = get_question_tree(input_data)
+    questions_path = get_question_tree(input_data).get_all_questions_path_data()
+    save_data(questions_path, current_dir / 'questions_path.json')
 
 
 if __name__ == '__main__':
--- third_task/questions_tree.py
+++ third_task/questions_tree.py
@@ -11,13 +11,13 @@ class Question(TypedDict):
     answers: list[Answer]
 
 
-class QuestionAndAnswer(TypedDict):
-    question: str
-
-
-class QuestionPaths(TypedDict):
+class QuestionsPathDetailedInfo(TypedDict):
     number: int
-    list: list[QuestionAndAnswer]
+    list: list[list[dict]]
+
+
+class QuestionsPath(TypedDict):
+    paths: QuestionsPathDetailedInfo
 
 
 class QuestionNode:
@@ -48,8 +48,35 @@ class QuestionNode:
             if question['question'] == question_text:
                 return question['answers']
 
+    def get_question_path(self) -> list[list[dict]]:
+        """
+        Возвращает данные о всех возможных путей опросов, начиная
+        с текущего вопроса.
+        """
+        end_answers = []
+        questions_path = []
+        for answer, question_node in self.answers.items():
+            if question_node is None:
+                end_answers.append(answer)
+                continue
+            for path in question_node.get_question_path():
+                questions_path.append([{self.question: answer}, *path])
+        if end_answers:
+            questions_path.append([{self.question: '/'.join(end_answers)}])
+        return questions_path
+
+    def get_all_questions_path_data(self) -> QuestionsPath:
+        """
+        Возвращает словарь с данными о всех возможных опросах
+        и их количество.
+        """
+        questions_path_list = self.get_question_path()
+        number = len(questions_path_list)
+        return {'paths': {'number': number, 'list': questions_path_list}}
+
     @classmethod
     def set_questions_list(cls, questions_list: list[Question]):
+        """Устанавливает список вопросов."""
         cls._questions_list = questions_list
 
     def __init__(self, question: str, answers: list[Answer]):
@@ -63,7 +90,7 @@ class QuestionNode:
         self.build(answers)
 
 
-def get_question_tree(questions: list[Question]):
+def get_question_tree(questions: list[Question]) -> QuestionNode:
     QuestionNode.set_questions_list(questions)
     question, answers = questions[0]['question'], questions[0]['answers']
     return QuestionNode(question, answers)
